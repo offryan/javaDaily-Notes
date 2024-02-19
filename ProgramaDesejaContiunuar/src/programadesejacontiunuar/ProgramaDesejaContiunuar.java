5a930b302c9232188fa7b7afe9ca650e5eaa2ac3
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-19 18:07:42 -0300

    While com Java
    While com Java

---
+++ ProgramaDesejaContiunuar/src/programadesejacontiunuar/ProgramaDesejaContiunuar.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+*/
+
+package programadesejacontiunuar;
+import java.util.Scanner;
+
+public class ProgramaDesejaContiunuar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int acumulador;
+        char resposta;
+
+        acumulador = 0;
+        resposta = 's';
+
+        while (resposta == 's' || resposta == 'S') {
+            System.out.printf("Deseja Contiunuar? s/n \n");
+            resposta = input.next().charAt(0);
+
+            System.out.printf("Acumulador: %d \n", acumulador);
+            acumulador = acumulador + 1;
+        }
+    }
+}
