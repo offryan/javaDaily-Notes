3e72379144f2a665ac880f383eea5a32498f02cb
DEV RYAN <85769101+offryan@users.noreply.github.com>
2024-02-19 18:04:39 -0300

    Contadores com Java
    Contadores com Java

---
+++ ProgramaContadorSimplesComFor/src/programacontadorsimplescomfor/ProgramaContadorSimplesComFor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programacontadorsimplescomfor;
+
+
+public class ProgramaContadorSimplesComFor {
+
+    
+    public static void main(String[] args) {
+        
+        int contador;
+
+        for (contador = 0; contador > 10; contador++){
+            System.out.printf("Contador: %d \n", contador);
+        }
+    }
+    
+}
